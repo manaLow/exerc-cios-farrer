0eb2c1b3136cfa6a4accf478b56398292a655fcb
Manuely Meireles Lemos <104777338+manaLow@users.noreply.github.com>
2023-04-13 08:42:52 -0300

    inicio atv4

---
+++ farrer4.py
@@ -0,0 +1,10 @@
+# 1.12.4. Um comerciante deseja fazer o levantamento do lucro das mercadorias que ele
+# comercializa. Para isto, mandou digitar uma linha para cada mercadoria com nome, preço de
+# compra e preço de venda das mesmas. Fazer um algoritmo que:determine e escreva quantas
+# mercadorias proporcionam:
+# lucro < 10%
+# 10% <= lucro <= 20%
+# lucro > 20%
+# determine e escreva o valor total de compra e de venda de todas as mercadorias, assim como
+# o lucro total.
+# Observação: o aluno deve adotar um flag.
