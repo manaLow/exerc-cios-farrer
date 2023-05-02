ab1ea10efe17c72592d278dce8620687b7dfd646
Manuely Meireles Lemos <manumeireleslemos@gmail.com>
2023-05-02 09:00:56 -0300

    Create farrer37.py

---
+++ farrer37.py
@@ -0,0 +1,20 @@
+def main():
+	# Declaração de Variáveis
+	num = float()
+	deno = float()
+	soma = float()
+	n = float()
+
+	fator = 1
+
+	for x in range(1,50):
+		
+		for i in range(1,n+1):
+			num = num*i
+
+
+
+
+
+if __name__ == "__main__":
+	main()
