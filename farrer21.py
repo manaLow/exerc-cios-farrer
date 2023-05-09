437ebfa3cfedca09ea56606fd47383881c60eb4d
Manuely Meireles Lemos <manumeireleslemos@gmail.com>
2023-05-09 08:56:06 -0300

    Create farrer21.py

---
+++ farrer21.py
@@ -0,0 +1,12 @@
+"""
+maiorsal = -1
+leia(num do op)
+	enquanto numop!=0 faça:
+		leia(quantd. de peças)
+		leia(sexo)
+		calcular o salario
+		escreva (nop,salario)
+		se salario > maiorsal:
+			maiorsal = salario
+			nopmaiorsal = nop
+"""
