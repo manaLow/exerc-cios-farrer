991d06a3e689cdb52f33dbd358267e11758d3f6e
Manuely Meireles Lemos <104777338+manaLow@users.noreply.github.com>
2023-05-16 14:12:27 -0300

    Update farrer21.py

--- farrer21.py
+++ farrer21.py
@@ -1,4 +1,25 @@
 """
+Numa fábrica trabalham homens e mulheres divididos em três classes:
+A – os que fazem até 30 peças por mês;
+B – os que fazem de 31 a 35 peças por mês;
+C – os que fazem mais de 35 peças por mês;
+A classe A recebe salário-mínimo. A classe B recebe salário-mínimo e mais 3% do salário
+mínimo por peça, acima das 30 iniciais. A classe C recebe salário-mínimo e mais 5% do salário
+mínimo por peça acima das 30 iniciais.
+Fazer um algoritmo que:
+a) leia várias linhas, contendo cada uma:
+- o número do operário;
+- o número de peças fabricadas por mês;
+- o sexo do operário;
+b) calcule e escreva
+- o salário de cada operário;
+- o total da folha mensal de pagamento da fábrica;
+- o número total de peças fabricadas por mês;
+- a média de peças fabricadas pelos homens em cada classe;
+- a média de peças fabricadas pelas mulheres em cada classe;
+- o número do operário ou operária de maior salário (não existe empate).
+Observação: A última linha, que servirá de flag, terá o número do operário igual a zero
+
 maiorsal = -1
 leia(num do op)
 	enquanto numop!=0 faça:
@@ -10,3 +31,14 @@ leia(num do op)
 			maiorsal = salario
 			nopmaiorsal = nop
 """
+
+def main():
+    
+	# Variáveis
+	pass
+
+
+
+
+if __name__ == "__main__":
+    main()
