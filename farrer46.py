f5ab410d2d9d19ff14f26183cf5d0723639741a9
Manuely Meireles Lemos <104777338+manaLow@users.noreply.github.com>
2023-05-21 16:55:11 -0300

    Update farrer46.py

--- farrer46.py
+++ farrer46.py
@@ -1,8 +1,33 @@
 def main():
 
 	# Declaração de variáveis
-	pass
+	y1 = float()
+	y2 = float()
+	y = float()
+	x = float()
 
+	# Inicializando variáveis
+	x = float(input())
+	y = float(input())
+
+	# Processamento
+	while x!= 0 and y!=0:
+		y1 = 3*x
+		y2 = (1/3)*x
+
+		if x > 0:
+			if y > y2 and y < y1:
+				print(f"Ponto: ({x},{y}) == INTERIOR") # Saída
+			else:
+				print(f"Ponto: ({x},{y}) == EXTERIOR") # Saída
+		else:
+			if y < y2 and y > y1:
+				print(f"Ponto: ({x},{y}) == INTERIOR") # Saída
+			else:
+				print(f"Ponto: ({x},{y}) == EXTERIOR") # Saída
+
+		x = float(input())
+		y = float(input())
 
 if __name__ == "__main__":
 	main()
