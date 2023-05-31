cf91c0aeea5458f0aa14f12b2ae538a8650c6ae9
Manuely Meireles Lemos <104777338+manaLow@users.noreply.github.com>
2023-05-31 13:48:29 -0300

    Create atividade15.py

---
+++ atividade15.py
@@ -0,0 +1,35 @@
+
+def f_hexadecimal(n):
+    
+    # Declaração de variáveis
+    
+
+
+    if n < 10:
+        res = f"{n}"
+    
+    if n == 10:
+        res = "A"
+    elif n == 11:
+        res = "B"
+    elif n == 12:
+        res = "C"
+    elif n == 13:
+        res = "D"
+    elif n == 14:
+        res = "E"
+    elif n == 15:
+        res = "F"
+    
+    if n >= 16:
+
+
+
+
+def main():
+
+
+
+
+if __name__ == "__main__":
+    main()
